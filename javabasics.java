e2455d255ec8848ccd7610438336a4733ceabc07
Anika Teja Reddy <anikatejareddy0009@gmail.come>
2026-07-06 16:42:11 +0530

    initial commit.

---
+++ javabasics.java
@@ -0,0 +1,14 @@
+public class javabasics{
+
+    public static void main(String arg[]){
+
+        System.out.println("hello world");
+
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hi");
+        System.out.println("hello");
+
+    }
+}
