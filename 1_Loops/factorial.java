e5f6ba0cc920e0e10b9a7f4cd27022bde747ad67
Anika Teja Reddy <anikatejareddy0009@gmail.come>
2026-07-07 21:59:23 +0530

    ADD Java practice programs

---
+++ 1_Loops/factorial.java
@@ -0,0 +1,12 @@
+
+public class factorial {
+    public static void main(String args[]){
+
+        Scanner c= new Scanner(System.in);
+        System.out.print("Enter the numbre ")
+
+        int n= sc.nextInt();
+
+    }
+    
+}
