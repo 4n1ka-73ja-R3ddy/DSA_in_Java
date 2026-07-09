e3cefb7d87d9ecbf020536a9aa1022d26cf010d2
Anika Teja Reddy <anikatejareddy0003@gmail.com>
2026-07-09 23:26:44 +0530

    functions module.

--- 3_functions/palindrome.java
+++ 3_functions/palindrome.java
@@ -1,3 +1,47 @@
-public class palindrome {
-    
+import java.util.*;
+
+public class palindrome
+{
+    public static void check_palindrome(int p)
+    {
+
+        int n = p;
+        int rev = 0;
+        int pow = 0;
+
+        while (n > 0)
+        {
+            int rem = n % 10;
+
+            rev = rev * 10 + rem;
+
+            pow++;
+            n = n / 10;
+
+        }
+
+        if (p == rev)
+        {
+            System.out.println("Number is palindrome");
+        }
+        else
+        {
+            System.out.println("Number is not palindrome");
+        }
+
+    }
+
+    public static void main(String args[])
+    {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the numbers");
+
+        int p = sc.nextInt();
+
+        check_palindrome(p);
+
+    }
+
 }
--- 3_functions/sum_of_digits_in_integer.java
+++ 3_functions/sum_of_digits_in_integer.java
@@ -1,3 +1,37 @@
-public class sum_of_digits_in_integer {
-    
+import java.util.*;
+
+public class sum_of_digits_in_integer
+{
+    public static void sum(int n)
+    {
+
+        int sum = 0;
+        int rem = 0;
+
+        while (n > 0)
+        {
+
+            rem = n % 10;
+            sum = sum + rem;
+
+            n = n / 10;
+        }
+
+        System.out.println("The sum of digits is: " + sum);
+
+    }
+
+    public static void main(String args[])
+    {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the number");
+
+        int n = sc.nextInt();
+
+        sum(n);
+
+    }
+
 }
